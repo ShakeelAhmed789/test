e5a16ff1250a81b7d4d817a6d5b76d43b0425d2a
Shakeel Ahmed <ahshakeel1994@gmail.com>
2021-05-26 03:28:56 +0500

    Add files via upload

---
+++ Login Application/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Write your JavaScript code.
+$("#target").submit(function (event) {
+
+    event.preventDefault();
+    var endpoint = "https://localhost:44331/api/Employee/";
+
+    var creds = {
+        username: $("#username").val(),
+        password: $("#password").val()
+    }
+
+    $('#validating').html('Sent Credentials to Server..');
+
+    $.ajax({
+        type: 'POST',
+        url: endpoint,
+        crossDomain: true,
+        dataType: 'jsonp',
+        contentType: 'application/json',
+        data: creds,
+        success: function (data) {
+        },
+        data: JSON.stringify(creds)
+    });
+});
